e6ed8e06bf874cbcb59cf895add65188ad4037e3
mac <mac@macdeimac.local>
2017-02-07 18:03:08 +0800

    Initial Commit

---
+++ 个人中心的效果/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  个人中心的效果
+//
+//  Created by mac on 17/2/7.
+//  Copyright © 2017年 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 个人中心的效果/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  个人中心的效果
+//
+//  Created by mac on 17/2/7.
+//  Copyright © 2017年 mac. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
